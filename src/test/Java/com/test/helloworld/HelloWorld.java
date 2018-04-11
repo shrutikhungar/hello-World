9b2df30c161cdd9d37f4150dcf51e97393b7a54a
shrutikhungar@gmail.com <shrutikhungar@gmail.com>
2018-04-11 09:24:52 +0530

    first commit

---
+++ src/test/Java/com/test/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+
+package com.test.helloworld;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld{
+	
+	@Test
+	public void hellowWorldFn() {
+		System.out.println("1000");
+		System.out.println("1001");
+		System.out.println("1002");
+		System.out.println("1003");
+		System.out.println("10004");
+	}
+}
